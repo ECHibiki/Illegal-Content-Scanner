8eeb6dae69e60ca3c4b26170fc336b0ded0e4803
ECHibiki <ecverniy@gmail.com>
2023-09-26 17:10:45 -0400

    Draft of functionality. Need to verify functionality and then implement side services(OCR and vichan-veriifcations)

--- index.js
+++ index.js
@@ -18,9 +18,24 @@
                 // Flag the post to be put into verification
     // Stores the most recent post ID it has seen for next loop
 
+
+    // Verify program follows URLs and properly uses regex to evaluate if a post is spam or not
+    // Clean up the program for readability sake
+    
+    // TODO: Add verification system to Vichan which will hide posts that are most likely illegal
+        // Verify that the bot works properly to flag posts and hide them from users
+        // Verify that reports are being sent when a post is flagged
+    // TODO: Add OCR system to check images for URLs and follow them/report them
+        // Verify that OCR is reading text on watermarked media
+        // (spammer uses https://watermarkly.com/ to watermark images)
+    
+    // TODO: Improve regex check/heuristics system for an in production system.
+    // Deploy to source
+
 const util = require('util')
 const https = require('https');
 const htmlparser2 = require('htmlparser2');
+const settings = require('./settings/settings');
 
 var requestToken = "";
 var lastRecognizedPost = "delete_";
@@ -30,15 +45,15 @@ login().then(() => {
     setInterval(() => {
         scanRecentPosts()
         console.error("Error on scan" , error)
-    }, SCAN_RATE_MS);
+    }, settings.SCAN_RATE_MS);
 })
 
 function startRecents(){
     console.log("starting recents");
     let r = https.request({
-        hostname: HEAD_URL,
+        hostname: settings.HEAD_URL,
         port: 443,
-        path: util.format(RECENT_POST_URL, 1),
+        path: util.format(settings.RECENT_POST_URL, 1),
         method: 'GET',
         headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
@@ -64,16 +79,16 @@ function startRecents(){
     r.end();
 }
 
-function scanRecentPosts(postQuantity = DEFAULT_RECENT_POSTS) {
+function scanRecentPosts(postQuantity = settings.DEFAULT_RECENT_POSTS) {
     if(postQuantity > 50) {
         console.log("Could not find last recognized post, so reattaining");
         startRecents();
         return
     }
     let r = https.request({
-        hostname: HEAD_URL,
+        hostname: settings.HEAD_URL,
         port: 443,
-        path: util.format(RECENT_POST_URL, postQuantity),
+        path: util.format(settings.RECENT_POST_URL, postQuantity),
         method: 'GET',
         headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
@@ -83,12 +98,12 @@ function scanRecentPosts(postQuantity = DEFAULT_RECENT_POSTS) {
         }, (res) => {
             const body = []
             res.on('data', (chunk) => body.push(chunk))
-            res.on('end', () => {
+            res.on('end', async () => {
                 const htmlBody = Buffer.concat(body).toString();
                 const dom = htmlparser2.parseDocument(htmlBody);
                 const del = htmlparser2.DomUtils.getElements( { id: lastRecognizedPost} , dom, true); 
                 if(!del.pop()){
-                    scanRecentPosts( postQuantity + DEFAULT_RECENT_POSTS )
+                    scanRecentPosts( postQuantity + settings.DEFAULT_RECENT_POSTS )
                     return
                 } else{
                     const posts = htmlparser2.DomUtils.getElements({ class: 'delete' }, dom, true);
@@ -98,7 +113,14 @@ function scanRecentPosts(postQuantity = DEFAULT_RECENT_POSTS) {
                         }
                         const scanPost = htmlparser2.DomUtils.getElements({class: "body"}, posts[i].parentNode.parentNode, true);
                         const board = posts[i].parentNode.parentNode.parentNode.attribs["data-board"]
-                        scanForReportWords(scanPost.pop() , posts[i].attribs.id , board)
+                        const textbody = htmlparser2.DomUtils.textContent( scanPost.pop() );
+                        const deleteID = posts[i].attribs.id
+                        let score = scanForReportWords(textbody ,);
+                        if(score < 5){
+                            score += await readIntoURLs(textbody , deleteID , board)
+                            score += await requestOCRForImage(deleteID , board)
+                        }
+                        decideAction(score, deleteID , board)
                     }
                     lastRecognizedPost = posts[0].attribs.id;
                     if (typeof(lastRecognizedPost) != "string" || !lastRecognizedPost.includes("delete_")){
@@ -115,63 +137,170 @@ function scanRecentPosts(postQuantity = DEFAULT_RECENT_POSTS) {
     
 }
 
+function decideAction(score , deleteID , board){
+    if( score  > 5 ){
+        // Here we will flag the post for verification
+        makeUnverified(deleteID , board);
+    }
+    if( score > 3 ){
+        // delete_10410
+        makeReport(deleteID , board);
+    }
+}
+
 function checkHeuristic(postText){
     let badCount = 0;
-    MALREGEX.forEach( (regex) => {
+    settings.MALREGEX.forEach( (regex) => {
         if (regex.test(postText)){
             badCount = 999;
         }
     });
-    MALWORDS.forEach( (word) => {
-        if (postText == (word)){
+    settings.MALWORDS.forEach( (word) => {
+        if (postText.includes(word)){
             badCount++;
         }
     })
     return badCount;
 }
 
-function scanForReportWords(post , deleteID , board){
-    console.log("Scanning post for report words ---" , htmlparser2.DomUtils.textContent( post ) , deleteID , board);
-    const TEXTBODY = htmlparser2.DomUtils.textContent( post );
-    if( checkHeuristic( TEXTBODY.toLowerCase() ) > 3 ){
-        // delete_10410
-        const REPORT = util.format(REPORT_REQUEST,  board , deleteID, deleteID, "Autoreport: Looks illegal");
-        console.log("reporting post" , REPORT , )
-        let r = https.request({
-            hostname: HEAD_URL,
+async function readIntoURLs(post , deleteID , board){
+    let urls = htmlparser2.DomUtils.getElementsByTagName("A", post, true);
+    let score = 0;
+    for(let i = 0 ; i < urls.length ; i++){
+        const url = urls[i];
+        if(url.attribs.href){
+            if(url.attribs.href.includes("http")){
+                console.log("Found URL" , url.attribs.href);
+                score += await new Promise( (recv) => {
+                    https.request({
+                        url: start_url,
+                        method: 'GET',
+                        followAllRedirects: true,
+                    }, function(res) {
+                        if(res.statusCode == 200){
+                            const body = []
+                            res.on('data', (chunk) => body.push(chunk))
+                            res.on('end', () => {
+                                const dom = htmlparser2.parseDocument(Buffer.concat(body).toString());
+                                let innerScore = this.checkHeuristic(dom.textContent , deleteID , board)
+                                recv(innerScore)
+                            })
+                        }
+                    });
+                } )
+            }
+        }
+    }
+    return score
+}
+
+function scanForReportWords(textbody ){
+    let score = checkHeuristic( textbody.toLowerCase() );
+    return score
+}
+
+function makeUnverified(deleteID , board){
+    const unverifification = util.format(settings.UNVERIFICATION_REQUEST,  board , deleteID);
+    console.log("flagging post" , unverified , )
+    let r = https.request({
+        hostname: settings.HEAD_URL,
+        port: 443,
+        path: settings.RECENT_REPORT_URL,
+        method: 'POST',
+        headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'Scan-Bot',
+            }
+        }, (res) => {
+            console.log('UNVERIFIED statusCode:', res.statusCode);
+            if(res.statusCode != 200){
+                const body = []
+                res.on('data', (chunk) => body.push(chunk))
+                res.on('end', () => {
+                    console.log(Buffer.concat(body).toString())
+                    throw("ERROR WHEN UNVERIFYING" , body)  
+                })
+            }
+        }
+    );
+    r.write(unverifification)
+    r.end();
+}
+// send request to GPU server to download and process an  image
+async function requestOCRForImage(deleteID, board){
+    console.log("unimplemented")
+    const score = await new Promise( (recv) => {
+        https.request({
+            hostname: settings.HEAD_URL,
             port: 443,
-            path: RECENT_REPORT_URL,
+            path: settings.OCR_SERVER_URL,
             method: 'POST',
             headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                     'User-Agent': 'Scan-Bot',
                 }
             }, (res) => {
-                console.log('REPORTED statusCode:', res.statusCode);
-                if(res.statusCode != 200){
-                    const body = []
-                    res.on('data', (chunk) => body.push(chunk))
+            console.log('UNVERIFIED statusCode:', res.statusCode);
+            if(res.statusCode != 200){
+                const body = []
+                res.on('data', (chunk) => body.push(chunk))
+
                     res.on('end', () => {
-                        console.log(Buffer.concat(body).toString())
-                        throw("ERROR WHEN REPORTING" , body)  
+                        console.log(Buffer.concat(body).toString())  
+                        const ocrText = Buffer.concat(body).toString()
+                        // TODO: Check the OCR text for URLs and report/flag them
+                        // TODO: Check the OCR text for spam words and report/flag them
+                        let ocrScore = scanForReportWords(ocrText );
+                        if(score < 5){
+                            ocrScore += readIntoURLs(ocrText , deleteID , board)
+                        }
+                        recv(ocrScore)
                     })
                 }
             }
         );
-        r.write(REPORT)
-        r.end();
-    }
+    })
+    r.write(unverifification)
+    r.end();
+    return score
+}
 
+function makeReport(deleteID , board){
+    const report = util.format(settings.REPORT_REQUEST,  board , deleteID, deleteID, "Autoreport: Looks illegal");
+    console.log("reporting post" , report , )
+    let r = https.request({
+        hostname: settings.HEAD_URL,
+        port: 443,
+        path: settings.RECENT_REPORT_URL,
+        method: 'POST',
+        headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'Scan-Bot',
+            }
+        }, (res) => {
+            console.log('REPORTED statusCode:', res.statusCode);
+            if(res.statusCode != 200){
+                const body = []
+                res.on('data', (chunk) => body.push(chunk))
+                res.on('end', () => {
+                    console.log(Buffer.concat(body).toString())
+                    throw("ERROR WHEN REPORTING" , body)  
+                })
+            }
+        }
+    );
+    r.write(report)
+    r.end();
 }
 
 async function login() {
-    const LOGIN = util.format(LOGIN_REQUEST, BOT_NAME, PASSWORD);
+    const login = util.format(settings.LOGIN_REQUEST, settings.BOT_NAME, settings.PASSWORD);
     // const LOGIN = LOGIN_REQUEST;
     return new Promise( (recv) => { 
         let r = https.request({
-        hostname: HEAD_URL,
+        hostname: settings.HEAD_URL,
         port: 443,
-        path: LOGIN_URL,
+        path: settings.LOGIN_URL,
         method: 'POST',
         headers: {
              'Content-Type': 'application/x-www-form-urlencoded',
@@ -181,7 +310,7 @@ async function login() {
             console.log('statusCode:', res.statusCode);
             console.log('headers:', res.headers);
             if (!res.headers['set-cookie']){
-                console.log("Request failed" , LOGIN)
+                console.log("Request failed" , settings.LOGIN)
                 const body = []
                 res.on('data', (chunk) => body.push(chunk))
                 res.on('end', () => {
@@ -194,7 +323,7 @@ async function login() {
                 recv();
             }
         });
-        r.write(LOGIN);
+        r.write(login);
         r.end();
     })    
 }
--- settings/settings.template.js
+++ settings/settings.template.js
@@ -1,5 +1,5 @@
 
-const MALWORDS = [
+ const  MALWORDS = [
     "PTHC",
     "CP",
     "CHILD",
@@ -11,21 +11,26 @@ const MALWORDS = [
     "LOLI",
 ]
 
-const MALREGEX = [
+ const  MALREGEX = [
     /^(?: |\n|^)(?:https?:\/\/)?(?:www\.)?[a-z0-9\-_]{1,}\.[a-z]{1,}\/[a-z0-9]+\/?$/ig
 ]
 
-const HEAD_URL = "kissu.moe";
+ const  HEAD_URL = "kissu.moe";
 
-const RECENT_REPORT_URL = "/post";
-const REPORT_REQUEST = "report=1&board=%s&%s=%s&reason=%s";
+ const  RECENT_REPORT_URL = "/post";
+ const  REPORT_REQUEST = "report=1&board=%s&%s=%s&reason=%s";
 
-const LOGIN_URL = "";
-const LOGIN_REQUEST = "username=%s&password=%s&login=Continue"
+ const  LOGIN_URL = "";
+ const  LOGIN_REQUEST = "username=%s&password=%s&login=Continue"
 
-const BOT_NAME = "Scan-Bot";
-const PASSWORD = "=";
+ const  BOT_NAME = "Scan-Bot";
+ const  PASSWORD = "=";
 
-const RECENT_POST_URL = "";
-const DEFAULT_RECENT_POSTS = 10;
-const SCAN_RATE_MS = 30 * 1000;
+ const  RECENT_POST_URL = "";
+ const  DEFAULT_RECENT_POSTS = 10;
+ const  SCAN_RATE_MS = 30 * 1000;
+
+ module.exports = {
+    MALWORDS, MALREGEX, HEAD_URL, RECENT_POST_URL, RECENT_REPORT_URL, REPORT_REQUEST, LOGIN_URL, LOGIN_REQUEST, 
+    BOT_NAME, PASSWORD, DEFAULT_RECENT_POSTS, SCAN_RATE_MS
+}
